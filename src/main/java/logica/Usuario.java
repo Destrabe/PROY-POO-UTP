5e27a0b4e41ca95e772b9d4e478f493e2b107c6f
Destrabe <marco96392@gmail.com>
2025-07-09 04:11:14 -0500

    ajustando la clase Usuario para incluir el rol

--- src/main/java/logica/Usuario.java
+++ src/main/java/logica/Usuario.java
@@ -3,33 +3,32 @@ package logica;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Usuario implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
     private String nombreUsuario;
     private String contrasenia;
+    
+    @ManyToOne
+    @JoinColumn(name="fk_rol")
+    private rol unRol;
 
     public Usuario() {
     }
 
-    public Usuario(int id, String nombreUsuario, String contrasenia) {
+    public Usuario(int id, String nombreUsuario, String contrasenia, rol unRol) {
         this.id = id;
         this.nombreUsuario = nombreUsuario;
         this.contrasenia = contrasenia;
-    }
-
-    public String getContrasenia() {
-        return contrasenia;
-    }
-
-    public void setContrasenia(String contrasenia) {
-        this.contrasenia = contrasenia;
+        this.unRol = unRol;
     }
 
     public int getId() {
@@ -39,7 +38,7 @@ public class Usuario implements Serializable {
     public void setId(int id) {
         this.id = id;
     }
-
+    
     public String getNombreUsuario() {
         return nombreUsuario;
     }
@@ -48,4 +47,19 @@ public class Usuario implements Serializable {
         this.nombreUsuario = nombreUsuario;
     }
     
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public rol getUnRol() {
+        return unRol;
+    }
+
+    public void setUnRol(rol unRol) {
+        this.unRol = unRol;
+    }
 }
