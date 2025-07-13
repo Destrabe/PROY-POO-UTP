70c66f69166fc6c79c172af077d5ef5f58b2f92a
Destrabe <marco96392@gmail.com>
2025-07-13 17:10:31 -0500

    arreglando lo que fregue

--- src/main/java/logica/main.java
+++ src/main/java/logica/main.java
@@ -4,6 +4,10 @@ import IGU.login;
 
 public class main {
     
+    // ya esta arreglado el problema de la base de datos
+    // ahora se puede ejecutar el programa sin problemas 
+    // :V
+    
     public static void main(String[] args) {
         login main=new login();
         main.setVisible(true);
